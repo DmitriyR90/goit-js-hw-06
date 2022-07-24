2f606d42587bb5da9c2a78f6d23177ad015168c3
DmitriyR90 <ryabokon.dn@gmail.com>
2022-07-24 20:27:38 +0300

    5+

--- js/task-05.js
+++ js/task-05.js
@@ -3,6 +3,11 @@ const nameOutput = document.querySelector('#name-output');
 
 nameInput.addEventListener('input', onInputName);
 
+let userNameDefault = nameOutput.textContent;
+
 function onInputName(event) {
   nameOutput.textContent = event.currentTarget.value;
+  if (nameOutput.textContent === '') {
+    nameOutput.textContent = userNameDefault;
+  }
 }
